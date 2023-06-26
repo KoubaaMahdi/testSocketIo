868adfd3d953a6c888c819c9ac1b5db8d23388fe
KoubaaMahdi <tikou_a@live.fr>
2023-06-26 09:36:50 +0100

    slt

--- Server/server.js
+++ Server/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http')
 const server = http.createServer(app);
-const {Server} =require('socket.io');
 const {instrument} = require('@socket.io/admin-ui')
-const path = require('path');
-const roomsList=[]
 app.engine('html',require('ejs').renderFile);
 app.set('view engine','html');
+const os = require('os');
+
+// Get the IP address
+
+const ip = 'http://172.16.0.195:4200';
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:4200",
+    origin: ip ,
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
@@ -21,58 +23,70 @@ instrument(io,{
   auth:false
 })
 app.use('/admin-ui', express.static(__dirname + '/socket.io-admin-ui/ui/dist'));
-// Serve the HTML file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname+"/login.html");
-});
-app.get('/choose', (req, res) => {
-  //res.sendFile(__dirname+"/index.html");
-  res.render(path.join(__dirname,'/','index.html'),{rooms:roomsList})
-});
-app.get('/room1', (req, res) => {
-  res.sendFile(__dirname+"/room1.html");
-});
-app.get('/room2', (req, res) => {
-  res.sendFile(__dirname+'/room2.html');
-});
-app.get('/admin', (req, res) => {
-  //res.sendFile(__dirname+'/admin.html');
-  res.render(path.join(__dirname,'/','admin.html'),{rooms:roomsList})
-});
-app.get('/room/', (req, res) => {
-  var name = req.query.name;
-  res.render(path.join(__dirname,'/','rooms.html'),{rooms:name})
-});
-app.get('/addRoom/', (req, res) => {
-  var name = req.query.name;
-  if(!roomsList.includes(name) )
-  roomsList.push(name);
-  res.sendStatus(200);
-});
-
 // Handle socket connection
+const connectedClients = new Map();
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.name;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+
+  // Vérifier si le nom d'utilisateur existe déjà dans les clients connectés
+  if (!connectedClients.has(username)) {
+    // Réutiliser le socket du client existant pour le même nom d'utilisateur
+    connectedClients.set(username,[socket.id])
+    
+    
+  }else{
+    connectedClients.get(username).push(socket.id);
+  }
+  socket.username=username
+  console.log(connectedClients)
+  next();
+  
+  
+})
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log("Connected")
   socket.on('join',(data)=>{
-    socket.join(data.room);
+    console.log("joined room",data)
+    socket.join(data);
   });
   // Handle incoming messages
   socket.on('chat message', (data) => {
     console.log('Message:', data);
-    //console.log('room:', data.room);
-    // Broadcast the message to all connected clients
-    io.emit('chat message', data);
+    socket.to(data.room).emit('chat message', data);
   });
   socket.on('fileUpload', (fileData) => {
     // Process the file data
     io.to(data.room).emit('Received file:', fileData);
   });
-
+  socket.on('private msg', (data) => {
+    testSockets = connectedClients.get(data.room)
+    testSockets.forEach(socketId => {
+      socket.to(socketId).emit('private msg', data);
+    });
+    testSockets = connectedClients.get(data.username)
+    
+    if(testSockets){
+      testSockets.forEach(socketId => {  
+      socket.to(socketId).emit('private msg', data);
+    });
+    }
+      
+    
+  });
   // Handle disconnect event
   socket.on('disconnect', () => {
+
+    connectedClients.delete(socket.username)
+    console.log(connectedClients)
     console.log('A user disconnected');
   });
 });
+
+
 
 // Start the server
 const port = 3000;
